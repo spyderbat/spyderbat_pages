7a90a9592a7621fc00d35b65ca0041468f9636be
opect.pol@i.ua <opect.pol@i.ua>
2022-01-05 00:48:33 +0200

    finish contact page

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,28 @@
 "use strict";
 "use strict";
+
 /**
-  * название функции
-  *
-  * @param {number} first - первое число
-  * @returns {number}
-  */
-"use strict";
+ * название функции
+ *
+ * @param {number} first - первое число
+ * @returns {number}
+ */
+var formSubmitBtn = document.querySelector('.contact__form-btn');
+var formCloseBtn = Array.from(document.querySelectorAll('.btn-close'));
+
+if (formSubmitBtn) {
+  formSubmitBtn.addEventListener('click', function () {
+    document.querySelector('.modal').classList.add('modal--open');
+    document.querySelector('body').style.overflow = 'hidden';
+  });
+}
+
+if (formCloseBtn.length > 0) {
+  formCloseBtn.forEach(function (element) {
+    element.addEventListener('click', function () {
+      document.querySelector('.modal').classList.remove('modal--open');
+      document.querySelector('body').style.overflow = 'auto';
+    });
+  });
+}
 //# sourceMappingURL=main.js.map
