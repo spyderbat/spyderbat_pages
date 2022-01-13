50672b699cd873366688ebd74411040760be0055
opect.pol@i.ua <opect.pol@i.ua>
2022-01-13 20:19:09 +0200

    test build v2

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,81 @@
-"use strict";var circles=document.querySelectorAll(".facts-element__circle");circles.forEach((function(e){if("true"==e.dataset.percentage){var t=e.querySelector(".progress"),r=e.querySelector(".facts-element__value"),o=t.getAttribute("r"),s=2*Math.PI*o,a=e.dataset.full,l=e.dataset.value,c=Math.floor(l/a*100);r.textContent=l,t.setAttribute("stroke-dasharray",s),t.setAttribute("stroke-dashoffset",s-s*c/100)}else{var n=e.querySelector(".progress"),u=e.querySelector(".facts-element__value"),d=n.getAttribute("r"),i=2*Math.PI*d,f=e.dataset.percent,m=Math.floor(f);u.textContent=f+"%",n.setAttribute("stroke-dasharray",i),n.setAttribute("stroke-dashoffset",i-i*m/100)}}));var swiper=new Swiper(".slider-home",{fadeEffect:{crossFade:!0},virtualTranslate:!0,speed:600,autoplayDisableOnInteraction:!0,slidersPerView:1,effect:"fade",spaceBetween:40,mousewheel:{releaseOnEdges:!0},rewind:!1,bounds:!0}),formSubmitBtn=document.querySelector(".contact__form-btn"),formCloseBtn=Array.from(document.querySelectorAll(".btn-close"));formSubmitBtn&&formSubmitBtn.addEventListener("click",(function(){document.querySelector(".modal").classList.add("modal--open"),document.querySelector("body").style.overflow="hidden"})),formCloseBtn.length>0&&formCloseBtn.forEach((function(e){e.addEventListener("click",(function(){document.querySelector(".modal").classList.remove("modal--open"),document.querySelector("body").style.overflow="auto"}))}));
+"use strict";
+"use strict";
+
+var circles = document.querySelectorAll('.facts-element__circle');
+circles.forEach(function (el) {
+  if (el.dataset.percentage == 'true') {
+    var progress = el.querySelector('.progress');
+    var valueBlock = el.querySelector('.facts-element__value');
+    var radius = progress.getAttribute('r');
+    var circleLength = 2 * Math.PI * radius;
+    var full = el.dataset.full;
+    var value = el.dataset.value;
+    var percentageProgress = Math.floor(value / full * 100);
+    valueBlock.textContent = value;
+    progress.setAttribute('stroke-dasharray', circleLength);
+    progress.setAttribute('stroke-dashoffset', circleLength - circleLength * percentageProgress / 100);
+  } else {
+    var _progress = el.querySelector('.progress');
+
+    var _valueBlock = el.querySelector('.facts-element__value');
+
+    var _radius = _progress.getAttribute('r');
+
+    var _circleLength = 2 * Math.PI * _radius;
+
+    var percent = el.dataset.percent;
+
+    var _percentageProgress = Math.floor(percent);
+
+    _valueBlock.textContent = percent + '%';
+
+    _progress.setAttribute('stroke-dasharray', _circleLength);
+
+    _progress.setAttribute('stroke-dashoffset', _circleLength - _circleLength * _percentageProgress / 100);
+  }
+});
+"use strict";
+
+var swiper = new Swiper('.slider-home', {
+  fadeEffect: {
+    crossFade: true
+  },
+  virtualTranslate: true,
+  speed: 600,
+  autoplayDisableOnInteraction: true,
+  slidersPerView: 1,
+  effect: "fade",
+  spaceBetween: 40,
+  mousewheel: {
+    releaseOnEdges: true
+  },
+  rewind: false,
+  bounds: true
+});
+"use strict";
+
+/**
+ * название функции
+ *
+ * @param {number} first - первое число
+ * @returns {number}
+ */
+var formSubmitBtn = document.querySelector('.contact__form-btn');
+var formCloseBtn = Array.from(document.querySelectorAll('.btn-close'));
+
+if (formSubmitBtn) {
+  formSubmitBtn.addEventListener('click', function () {
+    document.querySelector('.modal').classList.add('modal--open');
+    document.querySelector('body').style.overflow = 'hidden';
+  });
+}
+
+if (formCloseBtn.length > 0) {
+  formCloseBtn.forEach(function (element) {
+    element.addEventListener('click', function () {
+      document.querySelector('.modal').classList.remove('modal--open');
+      document.querySelector('body').style.overflow = 'auto';
+    });
+  });
+}
+//# sourceMappingURL=main.js.map
